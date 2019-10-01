c8ca4a49fa7e5c52217e9a3fee9916a561135fce
drillprops <drylbartosz@gmail.com>
2019-10-01 13:13:15 +0200

    create homepage

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './pages/Home/Home';
 
-const Index = () => <div>Hello</div>;
+const Index = () => {
+  return (
+    <Router>
+      <Route exact path='/' component={Home} />
+    </Router>
+  );
+};
 
 ReactDOM.render(<Index />, document.getElementById('root'));
