9ccbee21df6419e9cef8b2dddf132d34580230a2
drillprops <drylbartosz@gmail.com>
2019-10-01 13:50:42 +0200

    add google fonts to globalstyle

--- src/utils/globalstyle.ts
+++ src/utils/globalstyle.ts
@@ -1,11 +1,14 @@
 import { createGlobalStyle } from 'styled-components';
 
 export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css?family=Montserrat:500,600,700|Roboto+Condensed:700i&display=swap');
+
   body {
     margin: 0;
     padding:0;
+    font-family: 'Montserrat', sans-serif;
   }
-  
+
   html {
     box-sizing: border-box;
   }
