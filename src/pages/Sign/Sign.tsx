a0b1bfeca15bf9ddff1c813c9b5231a7e43502a0
drillprops <drylbartosz@gmail.com>
2019-10-04 13:44:41 +0200

    remove SignWrapper from Sign component, comment Register component temporarily

--- src/pages/Sign/Sign.tsx
+++ src/pages/Sign/Sign.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import { SignWrapper } from './Sign.styles';
 import Register from './Register/Register';
 import Login from './Login/Login';
 
 const Sign: React.FC = () => {
   return (
-    <SignWrapper>
+    <>
       <Login />
-      <Register />
-    </SignWrapper>
+      {/* <Register /> */}
+    </>
   );
 };
 
