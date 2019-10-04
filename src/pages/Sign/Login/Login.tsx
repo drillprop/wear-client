2f7aadc787cc30705f0b7549799e77e221bafe1d
drillprops <drylbartosz@gmail.com>
2019-10-04 13:46:02 +0200

    wrap Login and Register with SignWrapper, add SignHero

--- src/pages/Sign/Login/Login.tsx
+++ src/pages/Sign/Login/Login.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { SignTitle, SignForm, ForgotPassword } from '../Sign.styles';
+import {
+  SignTitle,
+  SignForm,
+  ForgotPassword,
+  SignWrapper
+} from '../Sign.styles';
 import Input from '../../../components/Input/Input';
 import mail from '../../../assets/mail-icon.svg';
 import padlock from '../../../assets/padlock-icon.svg';
 import Button from '../../../components/Button/Button';
+import SignHero from '../../../components/SignHero/SignHero';
+import woman from '../../../assets/young-woman-on-ferris-wheel.jpg';
 
 const Login = () => {
   return (
-    <SignForm>
-      <SignTitle>WELCOME BACK</SignTitle>
-      <Input
-        placeholder='user@example.com'
-        label='email'
-        icon={mail}
-        type='email'
-      />
-      <Input
-        placeholder='*********'
-        label='password'
-        icon={padlock}
-        type='password'
-      />
-      <ForgotPassword>Forgot your password?</ForgotPassword>
-      <Button type='submit'>login</Button>
-    </SignForm>
+    <SignWrapper>
+      <SignForm>
+        <SignTitle>WELCOME BACK</SignTitle>
+        <Input
+          placeholder='user@example.com'
+          label='email'
+          icon={mail}
+          type='email'
+        />
+        <Input
+          placeholder='*********'
+          label='password'
+          icon={padlock}
+          type='password'
+        />
+        <ForgotPassword>Forgot your password?</ForgotPassword>
+        <Button type='submit'>login</Button>
+      </SignForm>
+      <SignHero image={woman} />
+    </SignWrapper>
   );
 };
 
--- src/pages/Sign/Register/Register.tsx
+++ src/pages/Sign/Register/Register.tsx
@@ -1,29 +1,39 @@
 import React from 'react';
-import { SignForm, SignTitle } from '../Sign.styles';
+import { SignForm, SignTitle, SignWrapper } from '../Sign.styles';
 import Input from '../../../components/Input/Input';
 import Button from '../../../components/Button/Button';
 import mail from '../../../assets/mail-icon.svg';
 import user from '../../../assets/user-icon.svg';
+import blonde from '../../../assets/woman-playing-with-blonde-hair.jpg';
+import SignHero from '../../../components/SignHero/SignHero';
 
 const Register = () => {
   return (
-    <SignForm>
-      <SignTitle>CREATE NEW ACCOUNT</SignTitle>
-      <Input type='text' icon={user} label='username' placeholder='John Doe' />
-      <Input
-        type='email'
-        icon={mail}
-        label='email'
-        placeholder='user@example.com'
-      />
-      <Input
-        type='password'
-        icon={mail}
-        label='password'
-        placeholder='*******'
-      />
-      <Button type='submit'>register</Button>
-    </SignForm>
+    <SignWrapper>
+      <SignHero image={blonde} />
+      <SignForm>
+        <SignTitle>CREATE NEW ACCOUNT</SignTitle>
+        <Input
+          type='text'
+          icon={user}
+          label='username'
+          placeholder='John Doe'
+        />
+        <Input
+          type='email'
+          icon={mail}
+          label='email'
+          placeholder='user@example.com'
+        />
+        <Input
+          type='password'
+          icon={mail}
+          label='password'
+          placeholder='*******'
+        />
+        <Button type='submit'>register</Button>
+      </SignForm>
+    </SignWrapper>
   );
 };
 
