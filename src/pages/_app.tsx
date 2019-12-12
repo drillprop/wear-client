4e358de203b3acf973a4e57096652e506163fc2f
drillprops <drylbartosz@gmail.com>
2019-12-12 20:16:23 +0100

    install apollo-client

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,15 +2,24 @@ import React from 'react';
 import App from 'next/app';
 import Layout from '../components/Layout/Layout';
 import Globalstyle from '../utils/globalstyle';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
+import fetch from 'node-fetch';
+
+const client = new ApolloClient({
+  fetch: fetch as any
+});
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
-        <Globalstyle />
-        <Component {...pageProps} />
-      </Layout>
+      <ApolloProvider client={client}>
+        <Layout>
+          <Globalstyle />
+          <Component {...pageProps} />
+        </Layout>
+      </ApolloProvider>
     );
   }
 }
