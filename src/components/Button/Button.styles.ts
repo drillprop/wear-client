50020e13f72f85610375081e01d8f50bbcdf55cf
drillprops <drylbartosz@gmail.com>
2020-01-17 23:44:37 +0100

    change font and hover effect of button

--- src/components/Button/Button.styles.ts
+++ src/components/Button/Button.styles.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { fontLevel3 } from '../../utils/fontSizes';
+import { montserrat } from '../../utils/fonts';
 
 interface Props {
   mainColor?: string;
@@ -7,6 +8,9 @@ interface Props {
 }
 
 export const StyledButton = styled.button<Props>`
+  position: relative;
+  cursor: pointer;
+  font-family: ${montserrat};
   background: none;
   color: ${({ mainColor = 'black' }) => mainColor};
   outline: 3px solid ${({ mainColor = 'black' }) => mainColor};
@@ -15,10 +19,27 @@ export const StyledButton = styled.button<Props>`
   font-size: ${fontLevel3};
   font-weight: 700;
   padding: 10px 40px;
+  transition: color 150ms;
   :hover {
-    transition: color 150ms, background-color 150ms;
+    color: white;
+  }
+  ::after {
+    transform: scaleX(0);
+    transform-origin: right;
+    position: absolute;
+    content: '';
+    top: 0;
+    left: 0;
+    height: 100%;
+    width: 100%;
+    background-color: black;
+    transition: transform 300ms;
+  }
+  :hover ::after {
+    transform: scaleX(1);
+    z-index: -1;
+    transform-origin: left;
     color: white;
     cursor: pointer;
-    background-color: black;
   }
 `;
