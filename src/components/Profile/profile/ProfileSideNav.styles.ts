c81672b3b066121d0367a42e66027cbb10beeec8
drillprops <drylbartosz@gmail.com>
2020-01-21 12:51:29 +0100

    fill profile nav with mock content and style it

--- src/components/Profile/profile/ProfileSideNav.styles.ts
+++ src/components/Profile/profile/ProfileSideNav.styles.ts
@@ -1,7 +1,33 @@
 import styled from 'styled-components';
+import { montserrat, roboto } from '../../../utils/fonts';
+import { fontLevel2, fontLevel6 } from '../../../utils/fontSizes';
 
-export const ProfileNav = styled.nav``;
-export const ProfileNavTitle = styled.h1``;
-export const ProfileCard = styled.div``;
-export const ProfileNavList = styled.ul``;
-export const ProfileNavItem = styled.li``;
+export const ProfileNav = styled.nav`
+  top: 0;
+  margin-top: 110px;
+`;
+export const ProfileNavTitle = styled.h1`
+  font-family: ${roboto};
+  text-transform: uppercase;
+  letter-spacing: 4px;
+  font-size: ${fontLevel6};
+`;
+export const ProfileCard = styled.div`
+  background-color: #f5f5f5;
+  width: 90%;
+  padding: 20px;
+`;
+export const ProfileCardPar = styled.p`
+  font-family: ${montserrat};
+  font-size: ${fontLevel2};
+`;
+export const ProfileNavList = styled.ul`
+  margin-top: 60px;
+  padding: 0;
+`;
+export const ProfileNavItem = styled.li`
+  font-family: ${roboto};
+  margin-top: 12px;
+  text-transform: uppercase;
+  font-size: ${fontLevel6};
+`;
--- src/components/Profile/profile/ProfileSideNav.tsx
+++ src/components/Profile/profile/ProfileSideNav.tsx
@@ -1,8 +1,35 @@
 import React from 'react';
-import { ProfileNav } from './ProfileSideNav.styles';
+import {
+  ProfileNav,
+  ProfileNavTitle,
+  ProfileCard,
+  ProfileNavList,
+  ProfileNavItem,
+  ProfileCardPar
+} from './ProfileSideNav.styles';
+import LinkAnchor from '../../LinkAnchor/LinkAnchor';
 
 const ProfileSideNav = () => {
-  return <ProfileNav>Navigation</ProfileNav>;
+  return (
+    <ProfileNav>
+      <ProfileNavTitle>Email@Email.com</ProfileNavTitle>
+      <ProfileCard>
+        <ProfileCardPar>Total Orders: 0</ProfileCardPar>
+        <ProfileCardPar>Member since 20/12/2019</ProfileCardPar>
+      </ProfileCard>
+      <ProfileNavList>
+        <ProfileNavItem>
+          <LinkAnchor href='/profile'>profile</LinkAnchor>
+        </ProfileNavItem>
+        <ProfileNavItem>
+          <LinkAnchor href='/profile'>contact details</LinkAnchor>
+        </ProfileNavItem>
+        <ProfileNavItem>
+          <LinkAnchor href='/orders'>orders</LinkAnchor>
+        </ProfileNavItem>
+      </ProfileNavList>
+    </ProfileNav>
+  );
 };
 
 export default ProfileSideNav;
