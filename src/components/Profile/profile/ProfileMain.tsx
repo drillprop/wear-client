cc95a1ec8b6aba29a60eb17f365e0876d6a6416c
drillprops <drylbartosz@gmail.com>
2020-01-21 22:38:46 +0100

    add PersonalInforForm and ChangePasswordForm to ProfileMain

--- src/components/Profile/profile/ProfileMain.tsx
+++ src/components/Profile/profile/ProfileMain.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { ProfileMainWrapper, ProfileMainTitle } from './ProfileMain.styles';
+import PersonalInfoForm from './profilemain/PersonalInfoForm';
+import ChangePasswordForm from './profilemain/ChangePasswordForm';
 
 const ProfileMain = () => {
   return (
     <ProfileMainWrapper>
       <ProfileMainTitle>Your Profile</ProfileMainTitle>
+      <PersonalInfoForm />
+      <ChangePasswordForm />
     </ProfileMainWrapper>
   );
 };
