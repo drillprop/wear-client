96d2c7d0719af7b42e13e3f7d278316ef7e03dde
drillprops <drylbartosz@gmail.com>
2020-01-21 13:29:57 +0100

    create EditNameForm component

---
+++ src/components/Profile/profile/profilemain/EditNameForm.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import UserForm from '../../../UserForm/UserForm';
+
+const EditNameForm: React.FC = () => {
+  return <UserForm buttonText='save' onSubmit={() => {}}></UserForm>;
+};
+
+export default EditNameForm;
