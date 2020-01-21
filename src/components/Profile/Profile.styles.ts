72737d74129b24749edc796698b09b90ed171a05
drillprops <drylbartosz@gmail.com>
2020-01-21 18:10:31 +0100

    change grid values in ProfileWrapper

--- src/components/Profile/Profile.styles.ts
+++ src/components/Profile/Profile.styles.ts
@@ -2,5 +2,6 @@ import styled from 'styled-components';
 
 export const ProfileWrapper = styled.div`
   display: grid;
-  grid-template-columns: 350px 1fr;
+  grid-gap: 50px;
+  grid-template-columns: 280px 1fr;
 `;
