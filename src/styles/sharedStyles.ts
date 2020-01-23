a1e69ced7d4eae44721c05cebbeb847aa788c42c
drillprops <drylbartosz@gmail.com>
2020-01-23 11:53:22 +0100

    create sharedStyles file

---
+++ src/styles/sharedStyles.ts
@@ -0,0 +1,45 @@
+import styled from 'styled-components';
+import { roboto } from './fonts';
+
+export const AccountSiteWrapper = styled.div`
+  display: grid;
+  grid-gap: 90px;
+  grid-template-columns: 280px 1fr;
+`;
+
+export const AccountMain = styled.div`
+  form:first-of-type {
+    margin-top: 0;
+  }
+`;
+
+export const AccountForm = styled.form`
+  margin-top: 75px;
+  button {
+    width: 350px;
+  }
+  input {
+    width: 350px;
+  }
+`;
+
+export const AccountFormTitle = styled.h2`
+  margin: 0;
+  margin-top: 18px;
+  position: relative;
+  font-family: ${roboto};
+  text-transform: uppercase;
+  font-weight: 500;
+  margin-bottom: 30px;
+  ::after {
+    left: 0;
+    margin-top: 40px;
+    content: '';
+    position: absolute;
+    width: 100%;
+    height: 1px;
+    background-color: #dfdfdf;
+  }
+`;
+
+export const AccountParagraph = styled.p``;
