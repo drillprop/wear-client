6a2f68a702bc5455bbb4e06f356d0562f45bf407
drillprops <drylbartosz@gmail.com>
2020-01-27 22:50:35 +0100

    create AddressForm and implement update address

---
+++ src/components/ContactDetails/contactDetails/AddressForm.tsx
@@ -0,0 +1,115 @@
+import React, { useEffect, FormEvent } from 'react';
+import { AccountForm, AccountFormTitle } from '../../../styles/sharedStyles';
+import Input from '../../Input/Input';
+import Button from '../../Button/Button';
+import useForm from '../../../hooks/useForm';
+import { useMeQuery, useUpdateAddressMutation } from '../../../generated/types';
+import { ME } from '../../../graphql/queries';
+import ErrorMessage from '../../ErrorMessage/ErrorMessage';
+
+const AddressForm = () => {
+  const { values, handleInput, setForm } = useForm({
+    addressLine1: '',
+    addressLine2: '',
+    zipCode: '',
+    city: '',
+    country: ''
+  });
+  const { data } = useMeQuery();
+
+  const [updateAddress, { error, data: success }] = useUpdateAddressMutation({
+    refetchQueries: [
+      {
+        query: ME
+      }
+    ]
+  });
+
+  useEffect(() => {
+    if (data?.me?.address) {
+      const {
+        addressLine1,
+        addressLine2,
+        zipCode,
+        city,
+        country
+      } = data.me.address;
+
+      setForm({
+        addressLine1: addressLine1 || '',
+        addressLine2: addressLine2 || '',
+        zipCode: zipCode || '',
+        city: city || '',
+        country: country || ''
+      });
+    }
+  }, [data]);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    await updateAddress({
+      variables: {
+        ...values
+      }
+    });
+  };
+
+  return (
+    <AccountForm onSubmit={handleSubmit}>
+      <AccountFormTitle>Address</AccountFormTitle>
+      {success?.updateAddress.message}
+      <ErrorMessage error={error} />
+      <Input
+        value={values.addressLine1}
+        onChange={handleInput}
+        type='text'
+        placeholder='address line 1'
+        label='address line 1'
+        name='addressLine1'
+        icon='/home-icon.svg'
+        width='350px'
+      />
+      <Input
+        value={values.addressLine2}
+        onChange={handleInput}
+        type='text'
+        placeholder='address line 2'
+        label='address line 2'
+        name='addressLine2'
+        icon='/home-icon.svg'
+        width='350px'
+      />
+      <Input
+        value={values.zipCode}
+        onChange={handleInput}
+        type='text'
+        placeholder='00-000'
+        label='zip code'
+        name='zipCode'
+        icon='/city-icon.svg'
+        width='350px'
+      />
+      <Input
+        value={values.city}
+        onChange={handleInput}
+        type='text'
+        placeholder='City'
+        label='city'
+        icon='/city-icon.svg'
+        width='350px'
+      />
+      <Input
+        value={values.country}
+        onChange={handleInput}
+        type='text'
+        placeholder='Country'
+        label='country'
+        icon='/globe-icon.svg'
+        width='350px'
+      />
+      <Button type='submit'>save</Button>
+    </AccountForm>
+  );
+};
+
+export default AddressForm;
