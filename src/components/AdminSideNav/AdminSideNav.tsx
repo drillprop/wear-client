de3e83437862f2a6872212bd93f36363c7fea4ba
drillprops <drylbartosz@gmail.com>
2020-01-28 22:21:37 +0100

    create AdminSideNav component

---
+++ src/components/AdminSideNav/AdminSideNav.tsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import {
+  SideNavItem,
+  SideNavList,
+  SideNavMainTitle
+} from '../../styles/sharedStyles';
+import LinkAnchor from '../LinkAnchor/LinkAnchor';
+
+const AdminSideNav: React.FC = () => {
+  return (
+    <nav>
+      <SideNavMainTitle>ADMIN PANEL</SideNavMainTitle>
+      <SideNavList>
+        <SideNavItem>
+          <LinkAnchor highlight href='/admin/users'>
+            users
+          </LinkAnchor>
+        </SideNavItem>
+        <SideNavItem>
+          <LinkAnchor highlight href='/admin/items'>
+            items
+          </LinkAnchor>
+        </SideNavItem>
+        <SideNavItem>
+          <LinkAnchor highlight href='/admin/orders'>
+            user's orders
+          </LinkAnchor>
+        </SideNavItem>
+      </SideNavList>
+    </nav>
+  );
+};
+
+export default AdminSideNav;
