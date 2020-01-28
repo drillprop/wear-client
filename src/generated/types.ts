ca54a022568f7ff605c17d275c9deec4c9e36cff
drillprops <drylbartosz@gmail.com>
2020-01-28 23:35:29 +0100

    add users query and generate it's types

--- src/generated/types.ts
+++ src/generated/types.ts
@@ -419,6 +419,31 @@ export type MeQuery = (
   )> }
 );
 
+export type UsersQueryVariables = {
+  whereId?: Maybe<Scalars['ID']>,
+  take?: Maybe<Scalars['Int']>,
+  skip?: Maybe<Scalars['Int']>,
+  orderBy?: Maybe<Scalars['String']>,
+  desc?: Maybe<Scalars['Boolean']>,
+  whereRole?: Maybe<UserRole>,
+  whereEmail?: Maybe<Scalars['String']>,
+  whereFirstName?: Maybe<Scalars['String']>,
+  whereLastName?: Maybe<Scalars['String']>
+};
+
+
+export type UsersQuery = (
+  { __typename?: 'Query' }
+  & { users: Array<Maybe<(
+    { __typename?: 'User' }
+    & Pick<User, 'id' | 'email' | 'firstName' | 'lastName' | 'phoneNumber' | 'role'>
+    & { createdOrders: Array<Maybe<(
+      { __typename?: 'Order' }
+      & Pick<Order, 'id'>
+    )>> }
+  )>> }
+);
+
 
 export const RegisterDocument = gql`
     mutation Register($email: String!, $password: String!) {
@@ -740,3 +765,52 @@ export function useMeLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptio
 export type MeQueryHookResult = ReturnType<typeof useMeQuery>;
 export type MeLazyQueryHookResult = ReturnType<typeof useMeLazyQuery>;
 export type MeQueryResult = ApolloReactCommon.QueryResult<MeQuery, MeQueryVariables>;
+export const UsersDocument = gql`
+    query Users($whereId: ID, $take: Int, $skip: Int, $orderBy: String, $desc: Boolean, $whereRole: UserRole, $whereEmail: String, $whereFirstName: String, $whereLastName: String) {
+  users(input: {take: $take, skip: $skip, orderBy: $orderBy, desc: $desc, whereRole: $whereRole, whereEmail: $whereEmail, whereFirstName: $whereFirstName, whereLastName: $whereLastName}) {
+    id
+    email
+    firstName
+    lastName
+    phoneNumber
+    role
+    createdOrders {
+      id
+    }
+  }
+}
+    `;
+
+/**
+ * __useUsersQuery__
+ *
+ * To run a query within a React component, call `useUsersQuery` and pass it any options that fit your needs.
+ * When your component renders, `useUsersQuery` returns an object from Apollo Client that contains loading, error, and data properties 
+ * you can use to render your UI.
+ *
+ * @param baseOptions options that will be passed into the query, supported options are listed on: https://www.apollographql.com/docs/react/api/react-hooks/#options;
+ *
+ * @example
+ * const { data, loading, error } = useUsersQuery({
+ *   variables: {
+ *      whereId: // value for 'whereId'
+ *      take: // value for 'take'
+ *      skip: // value for 'skip'
+ *      orderBy: // value for 'orderBy'
+ *      desc: // value for 'desc'
+ *      whereRole: // value for 'whereRole'
+ *      whereEmail: // value for 'whereEmail'
+ *      whereFirstName: // value for 'whereFirstName'
+ *      whereLastName: // value for 'whereLastName'
+ *   },
+ * });
+ */
+export function useUsersQuery(baseOptions?: ApolloReactHooks.QueryHookOptions<UsersQuery, UsersQueryVariables>) {
+        return ApolloReactHooks.useQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions);
+      }
+export function useUsersLazyQuery(baseOptions?: ApolloReactHooks.LazyQueryHookOptions<UsersQuery, UsersQueryVariables>) {
+          return ApolloReactHooks.useLazyQuery<UsersQuery, UsersQueryVariables>(UsersDocument, baseOptions);
+        }
+export type UsersQueryHookResult = ReturnType<typeof useUsersQuery>;
+export type UsersLazyQueryHookResult = ReturnType<typeof useUsersLazyQuery>;
+export type UsersQueryResult = ApolloReactCommon.QueryResult<UsersQuery, UsersQueryVariables>;
--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -24,3 +24,40 @@ export const ME = gql`
     }
   }
 `;
+
+export const USERS = gql`
+  query Users(
+    $whereId: ID
+    $take: Int
+    $skip: Int
+    $orderBy: String
+    $desc: Boolean
+    $whereRole: UserRole
+    $whereEmail: String
+    $whereFirstName: String
+    $whereLastName: String
+  ) {
+    users(
+      input: {
+        take: $take
+        skip: $skip
+        orderBy: $orderBy
+        desc: $desc
+        whereRole: $whereRole
+        whereEmail: $whereEmail
+        whereFirstName: $whereFirstName
+        whereLastName: $whereLastName
+      }
+    ) {
+      id
+      email
+      firstName
+      lastName
+      phoneNumber
+      role
+      createdOrders {
+        id
+      }
+    }
+  }
+`;
