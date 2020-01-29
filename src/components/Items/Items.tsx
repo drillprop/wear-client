776c35a80050ecb0c174e0307e762eec323f35c9
drillprops <drylbartosz@gmail.com>
2020-01-29 23:37:58 +0100

    add ItemsTable and Pagination to Items component and pass apollo queries results to them via props

--- src/components/Items/Items.tsx
+++ src/components/Items/Items.tsx
@@ -1,31 +1,38 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { useItemsCountQuery, useItemsQuery } from '../../generated/types';
+import useChangePage from '../../hooks/useChangePage';
 import { SiteWrapper } from '../../styles/site.styles';
 import AdminSideNav from '../AdminSideNav/AdminSideNav';
+import Pagination from '../Pagination/Pagination';
+import ItemsTable from './items/ItemsTable';
 
 const Items: React.FC = () => {
-  const [pages, setPages] = useState({
-    take: 5,
-    skip: 0
-  });
+  const {
+    changePage,
+    pages: { skip, take }
+  } = useChangePage(5, 0);
 
-  const changePage = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const { textContent } = e.currentTarget;
-    if (textContent === '>')
-      setPages({
-        ...pages,
-        skip: pages.skip + pages.take
-      });
-    else
-      setPages({
-        ...pages,
-        skip: pages.skip - pages.take
-      });
-  };
+  const { data: countData, error: countError } = useItemsCountQuery();
+
+  const { data: itemsData, error: itemsError } = useItemsQuery({
+    variables: {
+      skip,
+      take
+    }
+  });
 
   return (
     <SiteWrapper>
       <AdminSideNav />
-      <div></div>
+      <div>
+        <ItemsTable items={itemsData?.items} />
+        <Pagination
+          changePage={changePage}
+          take={take}
+          skip={skip}
+          total={countData?.itemsCount}
+        />
+      </div>
     </SiteWrapper>
   );
 };
