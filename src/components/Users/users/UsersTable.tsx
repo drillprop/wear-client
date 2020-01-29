66d0310fb36d6d625ed785e6e1c398cd56972a85
drillprops <drylbartosz@gmail.com>
2020-01-29 11:04:46 +0100

    implement fetching users in UsersTable

--- src/components/Users/users/UsersTable.tsx
+++ src/components/Users/users/UsersTable.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useUsersQuery } from '../../../generated/types';
 import { SiteForm, SiteFormTitle } from '../../../styles/sharedStyles';
 import { StyledTable, TableBody } from './UsersTable.styles';
 import UserRow from './usersTable/UserRow';
 
 const UsersTable = () => {
+  const { data, error } = useUsersQuery({
+    variables: {
+      take: 5,
+      skip: 0
+    }
+  });
+
   return (
     <SiteForm>
       <SiteFormTitle>List of Users</SiteFormTitle>
@@ -18,7 +26,19 @@ const UsersTable = () => {
           </tr>
         </thead>
         <TableBody>
-          <UserRow email='email@email.com' role='ADMIN' id='SOME ID' />
+          {data?.users.map(user => {
+            if (user) {
+              return (
+                <UserRow
+                  key={user.id}
+                  email={user.email}
+                  role={user.role}
+                  id={user.id}
+                  fullName={`${user.firstName || ''} ${user.lastName || ''}`}
+                />
+              );
+            }
+          })}
         </TableBody>
       </StyledTable>
     </SiteForm>
