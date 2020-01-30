020dbb429756ef82bef64ca67bba849ef7908bd7
drillprops <drylbartosz@gmail.com>
2020-01-30 13:20:08 +0100

    add 6 gray colors to colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,2 +1,10 @@
 export const black = '#131313';
 export const white = '#F3F3F3';
+
+// grays
+export const gray1 = '#3B3B3B';
+export const gray2 = '#515151';
+export const gray3 = '#626262';
+export const gray4 = '#7E7E7E';
+export const gray5 = '#9E9E9E';
+export const gray6 = '#CFCFCF';
