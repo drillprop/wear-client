81ae36af28dce2f9200f4f4e3bf37917a37f7d59
drillprops <drylbartosz@gmail.com>
2020-01-31 22:55:11 +0100

    add one more color to colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -8,4 +8,5 @@ export const gray3 = '#626262';
 export const gray4 = '#7E7E7E';
 export const gray5 = '#9E9E9E';
 export const gray6 = '#CFCFCF';
-export const gray7 = '#F3F3F3;';
+export const gray7 = '#e3e3e3';
+export const gray8 = '#F3F3F3;';
