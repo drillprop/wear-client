659da77e4b9739224366ddeec1ae3bda68978cc4
drillprops <drylbartosz@gmail.com>
2020-02-04 17:02:05 +0100

    add CLOUDINARY_PRESET and CLOUDINARY_UPLOAD_URL to environment variables

--- next.config.js
+++ next.config.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 module.exports = {
   env: {
-    BACKEND_URL: process.env.BACKEND_URL
+    BACKEND_URL: process.env.BACKEND_URL,
+    CLOUDINARY_PRESET: process.env.CLOUDINARY_PRESET,
+    CLOUDINARY_UPLOAD_URL: process.env.CLOUDINARY_UPLOAD_URL
   }
 };
