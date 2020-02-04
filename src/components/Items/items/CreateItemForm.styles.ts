57f97952156cb63137d1ee10c559f376029e1a02
drillprops <drylbartosz@gmail.com>
2020-02-04 10:51:29 +0100

    create CreateItemForm.styles

---
+++ src/components/Items/items/CreateItemForm.styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const CreateItemWrapper = styled.div`
+  margin-top: 0;
+  display: grid;
+  grid-gap: 100px;
+  grid-template-columns: 350px 1fr;
+`;
