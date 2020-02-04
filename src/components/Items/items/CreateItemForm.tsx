2b39e8ca6cff279cf52913d3d48179916b173b56
drillprops <drylbartosz@gmail.com>
2020-02-04 10:54:04 +0100

    design CreateItemForm grid structure

--- src/components/Items/items/CreateItemForm.tsx
+++ src/components/Items/items/CreateItemForm.tsx
@@ -1,55 +1,76 @@
-import { SiteForm, SiteFormTitle } from '../../../styles/site.styles';
-import Input from '../../Input/Input';
-import Button from '../../Button/Button';
-import useForm from '../../../hooks/useForm';
-import Select from '../../Select/Select';
 import { Category, Gender } from '../../../generated/types';
+import useForm from '../../../hooks/useForm';
+import { SiteForm, SiteFormTitle } from '../../../styles/site.styles';
+import Button from '../../Button/Button';
+import Input from '../../Input/Input';
 import RadioGroup from '../../RadioGroup/RadioGroup';
+import Select from '../../Select/Select';
 import TextArea from '../../TextArea/TextArea';
+import { CreateItemWrapper } from './CreateItemForm.styles';
+import UploadImage from './createItemForm/UploadImage';
 
 const CreateItemForm = () => {
-  const { values, handleInput } = useForm({
-    name: ''
+  const { values, handleInput, setForm } = useForm({
+    name: '',
+    price: 0,
+    category: '',
+    gender: '',
+    description: '',
+    imageUrl: ''
   });
+
   return (
     <SiteForm>
       <SiteFormTitle>Create an item</SiteFormTitle>
-      <Input
-        type='text'
-        placeholder='name'
-        label='name'
-        name='name'
-        icon='/home-icon.svg'
-        width='350px'
-      />
-      <Input
-        type='number'
-        placeholder='price'
-        label='price'
-        name='price'
-        icon='/home-icon.svg'
-        width='350px'
-      />
-      <Select
-        options={Object.keys(Category)}
-        width='350px'
-        label='category'
-        placeHolder='select'
-      />
-      <RadioGroup
-        legend='Gender'
-        width='350px'
-        name='gender'
-        buttons={Object.keys(Gender)}
-      />
-      <TextArea
-        label='description'
-        placeholder='Lorem ipsum dolor sit amet.'
-        width='350px'
-      />
-      <Button width='350px' type='submit'>
-        save
-      </Button>
+      <CreateItemWrapper>
+        <UploadImage />
+        <div>
+          <Input
+            type='text'
+            placeholder='name'
+            onChange={handleInput}
+            value={values.name}
+            label='name'
+            name='name'
+            icon='/home-icon.svg'
+            width='350px'
+          />
+          <Input
+            type='number'
+            placeholder='price'
+            onChange={handleInput}
+            value={values.price}
+            label='price'
+            name='price'
+            icon='/home-icon.svg'
+            width='350px'
+          />
+          <Select
+            options={Object.keys(Category)}
+            width='350px'
+            label='category'
+            placeHolder='select'
+            onChange={category => setForm({ ...values, category })}
+          />
+          <RadioGroup
+            legend='Gender'
+            width='350px'
+            name='gender'
+            buttons={Object.keys(Gender)}
+            onChange={handleInput}
+          />
+          <TextArea
+            label='description'
+            placeholder='Lorem ipsum dolor sit amet.'
+            width='350px'
+            value={values.description}
+            onChange={handleInput}
+          />
+          <Button width='350px' type='submit'>
+            save
+          </Button>
+        </div>
+      </CreateItemWrapper>
     </SiteForm>
   );
 };
