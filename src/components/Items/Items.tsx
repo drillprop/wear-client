d078a3cb97ee7a640af1a96f1ecf9552a5738d11
drillprops <drylbartosz@gmail.com>
2020-02-08 15:28:33 +0100

    change itemsQuery fetch policy, update context variables in Items

--- src/components/Items/Items.tsx
+++ src/components/Items/Items.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { useItemsCountQuery, useItemsQuery } from '../../generated/types';
-import useChangePage from '../../hooks/useChangePage';
+import React, { useContext } from 'react';
+import { ItemsContext } from '../../contexts/Items.context';
+import { useItemsQuery, useItemsCountQuery } from '../../generated/types';
 import { SiteWrapper } from '../../styles/site.styles';
 import AdminSideNav from '../AdminSideNav/AdminSideNav';
 import Pagination from '../Pagination/Pagination';
@@ -8,17 +8,15 @@ import CreateItemForm from './items/CreateItemForm';
 import ItemsTable from './items/ItemsTable';
 
 const Items: React.FC = () => {
-  const { changePage, skip, take } = useChangePage(5, 0);
+  const { changePage, itemsQueryVariables } = useContext(ItemsContext);
 
   const { data: countData, error: countError } = useItemsCountQuery();
 
   const { data: itemsData, error: itemsError } = useItemsQuery({
     variables: {
-      orderBy: 'Item.createdAt',
-      desc: true,
-      skip,
-      take
-    }
+      ...itemsQueryVariables
+    },
+    fetchPolicy: 'cache-and-network'
   });
 
   return (
@@ -29,8 +27,8 @@ const Items: React.FC = () => {
         <ItemsTable items={itemsData?.items} />
         <Pagination
           changePage={changePage}
-          take={take}
-          skip={skip}
+          take={itemsQueryVariables.take}
+          skip={itemsQueryVariables.skip}
           total={countData?.itemsCount}
         />
       </div>
