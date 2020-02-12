643afeda3af0435bbe670ceebe56bfd327925a0c
drillprops <drylbartosz@gmail.com>
2020-02-12 22:18:18 +0100

    remove changeTake from useChangePage, replace it with useEffect

--- src/hooks/useChangePage.ts
+++ src/hooks/useChangePage.ts
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const useChangePage = (take: number, skip: number, total: number = 0) => {
   const [pageState, setPages] = useState({
@@ -8,6 +8,17 @@ const useChangePage = (take: number, skip: number, total: number = 0) => {
     pageNumber: Math.ceil(skip / take + 1 || 1),
     totalPages: Math.ceil(total / take)
   });
+
+  useEffect(() => {
+    setPages({
+      ...pageState,
+      take,
+      skip:
+        take >= pageState.total ? Math.max(0, total - take) : pageState.skip,
+      pageNumber: Math.ceil(pageState.skip / take + 1 || 1),
+      totalPages: Math.ceil(pageState.total / take)
+    });
+  }, [take, total]);
 
   const changePage = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const { textContent } = e.currentTarget;
@@ -31,22 +42,10 @@ const useChangePage = (take: number, skip: number, total: number = 0) => {
     }
   };
 
-  const changeTake = (take: number) => {
-    setPages({
-      ...pageState,
-      take,
-      skip:
-        take >= pageState.total ? Math.max(0, total - take) : pageState.skip,
-      pageNumber: Math.ceil(pageState.skip / take + 1 || 1),
-      totalPages: Math.ceil(pageState.total / take)
-    });
-  };
-
   return {
     skip: pageState.skip,
     take: pageState.take,
-    changePage,
-    changeTake
+    changePage
   };
 };
 
