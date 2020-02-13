59a37ffbf06469ed8761905e06208a8692bd9eea
drillprops <drylbartosz@gmail.com>
2020-02-13 13:48:46 +0100

    create basic SingleItem html structure, create SingleItem.styles

---
+++ src/components/SingleItem/SingleItem.styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const SingleItemMain = styled.div``;
+
+export const SingleItemImg = styled.img``;
+
+export const SingleItemInfo = styled.section``;
+export const SingleItemName = styled.h1``;
+export const SingleItemDescription = styled.p``;
+export const SingleItemPrice = styled.div``;
+export const AddToCart = styled.div``;
+export const SingleItemEdit = styled.span``;
--- src/components/SingleItem/SingleItem.tsx
+++ src/components/SingleItem/SingleItem.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
-import { useSingleItemQuery } from '../../generated/types';
+import {
+  useMeQuery,
+  UserRole,
+  useSingleItemQuery
+} from '../../generated/types';
+import { SiteSubtitle, SiteWrapper } from '../../styles/site.styles';
+import {
+  AddToCart,
+  SingleItemDescription,
+  SingleItemImg,
+  SingleItemMain,
+  SingleItemName,
+  SingleItemPrice,
+  SingleItemInfo,
+  SingleItemEdit
+} from './SingleItem.styles';
 
 interface Props {
   query: {
@@ -8,13 +23,34 @@ interface Props {
 }
 
 const SingleItem: React.FC<Props> = ({ query }) => {
+  const meQuery = useMeQuery();
+  const isAdmin =
+    meQuery.data?.me && meQuery.data.me.role !== UserRole.Customer;
   const { data } = useSingleItemQuery({
     variables: {
       id: query.id
     }
   });
-  console.log(data?.item);
-  return <div>single item {query.id}</div>;
+  return (
+    <SiteWrapper>
+      <nav />
+      <div>
+        <SiteSubtitle>{data?.item?.category}</SiteSubtitle>
+        <SingleItemMain>
+          <SingleItemImg src={data?.item?.imageUrl} alt={data?.item?.name} />
+          <SingleItemInfo>
+            <SingleItemName>{data?.item?.name}</SingleItemName>
+            <SingleItemDescription>
+              {data?.item?.description}
+            </SingleItemDescription>
+            <SingleItemPrice>$ {data?.item?.price}</SingleItemPrice>
+            <AddToCart>add to cart</AddToCart>
+            {isAdmin ? <SingleItemEdit>edit</SingleItemEdit> : null}
+          </SingleItemInfo>
+        </SingleItemMain>
+      </div>
+    </SiteWrapper>
+  );
 };
 
 export default SingleItem;
