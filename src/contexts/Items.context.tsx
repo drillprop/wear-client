864e0331760f206405539d10c4c0cbad3646fdcc
drillprops <drylbartosz@gmail.com>
2020-02-22 14:32:18 +0100

    remove useEffects and changePage in Items and Users contexts,

--- src/contexts/Items.context.tsx
+++ src/contexts/Items.context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import {
   ItemsQuery,
   ItemsQueryVariables,
@@ -8,7 +8,6 @@ import {
 interface Context {
   variables: ItemsQueryVariables;
   setVariables: (variables: any | ItemsQueryVariables) => void;
-  changePage: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
   items: ItemsQuery['items']['select'];
   count: number;
 }
@@ -16,14 +15,12 @@ interface Context {
 export const ItemsContext = createContext<Context>({
   variables: {},
   setVariables: () => {},
-  changePage: () => {},
   items: [],
   count: 0
 });
 
 const ItemsProvider: React.FC = ({ children }) => {
   const [variables, setVariables] = useState({ take: 5, skip: 0 });
-  const { skip, take } = variables;
 
   const { data } = useItemsQuery({
     variables,
@@ -32,36 +29,11 @@ const ItemsProvider: React.FC = ({ children }) => {
   const count = data?.items.count || 0;
   const items = data?.items.select || [];
 
-  useEffect(() => {
-    setVariables({
-      ...variables,
-      skip: take >= count ? Math.max(0, count - take) : skip
-    });
-  }, [take, count]);
-
-  const changePage = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const { textContent } = e.currentTarget;
-
-    if (textContent === '>') {
-      setVariables({
-        ...variables,
-        skip: count > skip + take ? skip + take : skip
-      });
-    }
-    if (textContent === '<') {
-      setVariables({
-        ...variables,
-        skip: Math.max(0, skip - take)
-      });
-    }
-  };
-
   return (
     <ItemsContext.Provider
       value={{
         variables,
         setVariables,
-        changePage,
         items,
         count
       }}
--- src/contexts/Users.context.tsx
+++ src/contexts/Users.context.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { createContext, useState } from 'react';
 import {
   UsersQuery,
   UsersQueryVariables,
@@ -8,7 +8,6 @@ import {
 interface Context {
   variables: UsersQueryVariables;
   setVariables: (variables: any | UsersQueryVariables) => void;
-  changePage: (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
   users: UsersQuery['users']['select'];
   count: UsersQuery['users']['count'];
 }
@@ -16,52 +15,26 @@ interface Context {
 export const UsersContext = createContext<Context>({
   variables: {},
   setVariables: () => {},
-  changePage: () => {},
   users: [],
   count: 0
 });
 
 const UsersProvider: React.FC = ({ children }) => {
   const [variables, setVariables] = useState({ take: 5, skip: 0 });
-  const { skip, take } = variables;
 
   const { data } = useUsersQuery({
     variables,
     fetchPolicy: 'cache-and-network'
   });
+
   const count = data?.users.count || 0;
   const users = data?.users.select || [];
-
-  useEffect(() => {
-    setVariables({
-      ...variables,
-      skip: take >= count ? Math.max(0, count - take) : skip
-    });
-  }, [take, count]);
-
-  const changePage = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
-    const { textContent } = e.currentTarget;
-
-    if (textContent === '>') {
-      setVariables({
-        ...variables,
-        skip: count > skip + take ? skip + take : skip
-      });
-    }
-    if (textContent === '<') {
-      setVariables({
-        ...variables,
-        skip: Math.max(0, skip - take)
-      });
-    }
-  };
 
   return (
     <UsersContext.Provider
       value={{
         variables,
         setVariables,
-        changePage,
         users,
         count
       }}
