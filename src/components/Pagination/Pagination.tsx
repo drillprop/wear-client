cabeeb7e9202e00ae7d65d28409b7db380cfd2bd
drillprops <drylbartosz@gmail.com>
2020-02-22 14:36:54 +0100

    in Pagination set skip to 0 if take change, remove redundant code and props

--- src/components/Pagination/Pagination.tsx
+++ src/components/Pagination/Pagination.tsx
@@ -7,44 +7,34 @@ import {
 
 interface Props {
   total?: number;
-  take: number;
-  skip: number;
   setNewState: React.Dispatch<React.SetStateAction<any>>;
   state?: any;
 }
 
-const Pagination: React.FC<Props> = ({
-  total = 0,
-  take,
-  skip,
-  setNewState,
-  state
-}) => {
+const Pagination: React.FC<Props> = ({ total = 0, setNewState, state }) => {
+  const { take, skip } = state;
   const pageNumber = Math.ceil(skip / take + 1 || 1);
   const totalPages = Math.ceil(total / take);
 
   useEffect(() => {
     setNewState({
       ...state,
-      skip: take >= total ? Math.max(0, total - take) : skip
+      skip: 0
     });
   }, [take, total]);
 
   const changePage = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
     const { textContent } = e.currentTarget;
-
-    if (textContent === '>') {
+    if (textContent === '>')
       setNewState({
         ...state,
         skip: total > skip + take ? skip + take : skip
       });
-    }
-    if (textContent === '<') {
+    if (textContent === '<')
       setNewState({
         ...state,
         skip: Math.max(0, skip - take)
       });
-    }
   };
 
   return totalPages >= 1 ? (
