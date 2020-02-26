8e9a3783b9f5ef81ecced24e28bbe6ae414d3e50
drillprops <drylbartosz@gmail.com>
2020-02-26 14:55:03 +0100

    change style of active LinkAnchor

--- src/components/LinkAnchor/LinkAnchor.styles.ts
+++ src/components/LinkAnchor/LinkAnchor.styles.ts
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
+import { grays } from '../../styles/colors';
 
 interface Props {
   active?: boolean;
 }
 
 export const StyledAnchor = styled.a<Props>`
-  text-decoration: ${props => (props.active ? 'underline' : 'none')};
   cursor: pointer;
+  font-weight: ${props => (props.active ? 700 : 400)};
+  color: ${props => (props.active ? grays[0] : 'inherit')};
 `;
