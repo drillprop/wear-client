959d790b4dd1cad4d3b7fb641393817d4903aff0
drillprops <drylbartosz@gmail.com>
2020-03-12 16:34:23 +0100

    create AdminItem page

---
+++ src/pages/admin/item.tsx
@@ -0,0 +1,37 @@
+import { ApolloQueryResult } from 'apollo-boost';
+import { NextPage } from 'next';
+import { ApolloAppContext } from 'next-with-apollo';
+import Head from 'next/head';
+import EditItemForm from '../../components/EditItemForm/EditItemForm';
+import { SingleItemQuery } from '../../generated/types';
+import SINGLE_ITEM from '../../graphql/queries/SINGLE_ITEM';
+
+interface Props {
+  singleItemQuery: ApolloQueryResult<SingleItemQuery>;
+  query?: {
+    category?: string;
+  };
+}
+
+const AdminItem: NextPage<Props> = ({ singleItemQuery, query }) => {
+  const { data } = singleItemQuery;
+  console.log(query);
+  return (
+    <>
+      <Head>
+        <title> wear {data.item?.name && `| ${data.item?.name}`}</title>
+      </Head>
+      <EditItemForm item={data.item} />;
+    </>
+  );
+};
+
+AdminItem.getInitialProps = async (ctx: ApolloAppContext) => {
+  const singleItemQuery = await ctx.apolloClient.query<SingleItemQuery>({
+    query: SINGLE_ITEM,
+    variables: ctx.query
+  });
+  return { singleItemQuery };
+};
+
+export default AdminItem;
