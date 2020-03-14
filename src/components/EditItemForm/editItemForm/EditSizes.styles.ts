73e3adc78bc26511f72f73f30add71f32078b06e
drillprops <drylbartosz@gmail.com>
2020-03-14 20:30:52 +0100

    create EditSizes component

---
+++ src/components/EditItemForm/editItemForm/EditSizes.styles.ts
@@ -0,0 +1,33 @@
+import styled from 'styled-components';
+import { grays, white } from '../../../styles/colors';
+import { roboto } from '../../../styles/fonts';
+import fontSizes from '../../../styles/fontSizes';
+
+export const SizesInputsWrapper = styled.div`
+  position: relative;
+  margin: 0;
+  padding: 0;
+  margin-top: 34px;
+  width: 100%;
+  border: 2px solid ${grays[0]};
+  display: grid;
+  padding: 30px 25px;
+  grid-gap: 15px;
+  justify-items: center;
+  grid-template-columns: repeat(2, 1fr);
+  ::before {
+    position: absolute;
+    top: -8px;
+    left: 5px;
+    content: 'available sizes';
+    background-color: ${white};
+    padding: 0;
+    padding-right: 5px;
+    padding-left: 5px;
+    font-family: ${roboto};
+    font-weight: 700;
+    font-size: ${fontSizes[1]};
+    color: ${grays[0]};
+    text-transform: uppercase;
+  }
+`;
---
+++ src/components/EditItemForm/editItemForm/EditSizes.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { SizeSymbol } from '../../../generated/types';
+import Input from '../../Input/Input';
+import { SizesInputsWrapper } from './EditSizes.styles';
+
+interface Props {
+  setForm: React.Dispatch<any>;
+  sizes: any;
+}
+
+const EditSizes: React.FC<Props> = ({ sizes, setForm }) => {
+  const handleOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const newSizes = {
+      ...sizes,
+      [e.target.name]: parseInt(e.target.value)
+    };
+    setForm((values: any) => ({
+      ...values,
+      sizes: newSizes
+    }));
+  };
+  return (
+    <SizesInputsWrapper>
+      {Object.values(SizeSymbol).map(size => (
+        <Input
+          key={size}
+          name={size}
+          icon='/category-icon.svg'
+          width='90px'
+          type='number'
+          marginTop='0'
+          placeholder='0'
+          value={sizes[size]}
+          label={size}
+          onChange={handleOnChange}
+        />
+      ))}
+    </SizesInputsWrapper>
+  );
+};
+
+export default EditSizes;
