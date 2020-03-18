69de0f31af5217987430b4a2a491ce735de79301
drillprops <drylbartosz@gmail.com>
2020-03-18 18:38:57 +0100

    create dropdownmenu in CartDropdown

--- src/components/Layout/layout/header/headerNav/CartDropdown.styles.ts
+++ src/components/Layout/layout/header/headerNav/CartDropdown.styles.ts
@@ -1,1 +1,73 @@
 import styled from 'styled-components';
+import { white, grays, black } from '../../../../../styles/colors';
+import fontSizes from '../../../../../styles/fontSizes';
+import { montserrat } from '../../../../../styles/fonts';
+
+export const CartDropDownWrapper = styled.div`
+  position: absolute;
+  right: 0;
+  background-color: ${white};
+`;
+
+export const CartDropdownList = styled.ul`
+  padding: 0;
+`;
+
+export const CartDropdownItem = styled.li`
+  position: relative;
+  :first-child {
+    padding-top: 40px;
+  }
+  padding-left: 25px;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  display: grid;
+  grid-gap: 20px;
+  grid-template-columns: repeat(2, 90px);
+  height: 100%;
+  ::after {
+    position: absolute;
+    bottom: 0;
+    content: '';
+    height: 1px;
+    width: 100%;
+    background-color: ${grays[5]};
+  }
+`;
+
+export const CartItemImg = styled.img`
+  object-fit: cover;
+  width: 90px;
+  height: 120px;
+  margin: 0;
+`;
+export const CartItemInfo = styled.div`
+  letter-spacing: 0;
+  font-family: ${montserrat};
+  font-size: ${fontSizes[0]};
+  color: ${grays[0]};
+`;
+export const CartItemName = styled.div`
+  color: ${black};
+  letter-spacing: 0;
+  font-weight: 700;
+`;
+export const CartItemSize = styled.div`
+  margin-top: 30px;
+`;
+export const CartItemPrice = styled.div`
+  margin-top: 20px;
+  font-weight: 700;
+`;
+
+export const CartDropdownButton = styled.button`
+  font-family: ${montserrat};
+  cursor: pointer;
+  font-size: ${fontSizes[1]};
+  display: block;
+  margin: 10px auto;
+  background-color: ${black};
+  border: none;
+  padding: 10px 30px;
+  color: ${white};
+`;
--- src/components/Layout/layout/header/headerNav/CartDropdown.tsx
+++ src/components/Layout/layout/header/headerNav/CartDropdown.tsx
@@ -1,23 +1,57 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useState } from 'react';
 import { useCart } from '../../../../../contexts/CartContext';
-import LinkAnchor from '../../../../LinkAnchor/LinkAnchor';
-import CartIcon from '../../../../CartIcon/CartIcon';
 import { black } from '../../../../../styles/colors';
+import CartIcon from '../../../../CartIcon/CartIcon';
+import LinkAnchor from '../../../../LinkAnchor/LinkAnchor';
 import { Li } from '../HeaderNav.styles';
+import {
+  CartDropdownButton,
+  CartDropdownItem,
+  CartDropdownList,
+  CartDropDownWrapper,
+  CartItemImg,
+  CartItemInfo,
+  CartItemName,
+  CartItemPrice,
+  CartItemSize
+} from './CartDropdown.styles';
 
 const CartDropdown = () => {
-  const { cartItems } = useCart();
+  const { cartItems, cartVisible, toggleCartVisible } = useCart();
   const cartItemsLength = cartItems.length;
   const products = cartItemsLength
     ? `${cartItemsLength} product${cartItemsLength > 1 ? 's' : ''} in `
     : '';
   return (
-    <Li>
+    <Li
+      onMouseEnter={() => toggleCartVisible(true)}
+      onMouseLeave={() => toggleCartVisible(false)}
+    >
       <LinkAnchor href='/cart' queryHighlight={!!cartItemsLength}>
         <CartIcon fill={cartItemsLength ? black : 'none'} />
         {products}
         cart
       </LinkAnchor>
+      {cartVisible && !!cartItemsLength && (
+        <CartDropDownWrapper>
+          <CartDropdownList>
+            {cartItems.map(item => (
+              <CartDropdownItem key={item.id}>
+                <CartItemImg src={item.imageUrl} alt='girl' />
+                <CartItemInfo>
+                  <CartItemName>{item.name}</CartItemName>
+                  <CartItemSize>{item.size}</CartItemSize>
+                  <CartItemPrice>${item.price}</CartItemPrice>
+                </CartItemInfo>
+              </CartDropdownItem>
+            ))}
+          </CartDropdownList>
+          <Link href='/cart'>
+            <CartDropdownButton>go to cart</CartDropdownButton>
+          </Link>
+        </CartDropDownWrapper>
+      )}
     </Li>
   );
 };
