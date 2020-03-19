3b75383f51bfc947c7f32b91cf77c7b5253cdff8
drillprops <drylbartosz@gmail.com>
2020-03-19 01:15:49 +0100

    create getCartTotals util in CartContext.utils

--- src/contexts/CartContext.utils.ts
+++ src/contexts/CartContext.utils.ts
@@ -13,3 +13,15 @@ export const addItem = (items: CartItem[], itemToAdd: CartItem): CartItem[] => {
   }
   return [{ ...itemToAdd, quantity: 1 }, ...items];
 };
+
+export const getCartTotals = (items: CartItem[]) => {
+  if (!items) return { total: 0, totalPrice: 0 };
+  return items.reduce(
+    (acc, item) => {
+      acc.total = acc.total + item.quantity;
+      acc.totalPrice = acc.totalPrice + item.price * item.quantity;
+      return acc;
+    },
+    { total: 0, totalPrice: 0 }
+  );
+};
