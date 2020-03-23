724d2113491e07f8cff2d225a4fc6d35dd225da2
drillprops <drylbartosz@gmail.com>
2020-03-23 01:52:11 +0100

    create [gender] folder in pages with [category], index and item file

---
+++ src/pages/[gender]/[category].tsx
@@ -0,0 +1,19 @@
+import { NextPage } from 'next';
+import React from 'react';
+import Shop from '../../components/Shop/Shop';
+import { Gender } from '../../generated/types';
+
+interface Props {
+  query: {
+    gender?: string;
+    category?: string;
+    page?: string;
+  };
+}
+
+const ShopPage: NextPage<Props> = ({ query }) => {
+  console.log(query);
+  return <Shop gender={Gender.MAN} query={query} />;
+};
+
+export default ShopPage;
---
+++ src/pages/[gender]/index.tsx
@@ -0,0 +1,17 @@
+import { NextPage } from 'next';
+import React from 'react';
+import Shop from '../../components/Shop/Shop';
+import { Gender } from '../../generated/types';
+
+interface Props {
+  query: {
+    category?: string;
+    page?: string;
+  };
+}
+
+const ShopPage: NextPage<Props> = ({ query }) => {
+  return <Shop gender={Gender.MAN} query={query} />;
+};
+
+export default ShopPage;
