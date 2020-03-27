3234a3a8e723f3b23f541e3f226efceada3272db
drillprops <drylbartosz@gmail.com>
2020-03-27 02:36:43 +0100

    add gender property in convertOrderedItems

--- src/utils/convertOrderedItems.ts
+++ src/utils/convertOrderedItems.ts
@@ -1,8 +1,8 @@
-import { Item, Ordered_Item, SizeSymbol } from '../generated/types';
+import { Item, Ordered_Item, SizeSymbol, Gender } from '../generated/types';
 
 type OrderedItems = Array<
   Pick<Ordered_Item, 'id' | 'sizeSymbol'> & {
-    item: Pick<Item, 'name' | 'price' | 'id'>;
+    item: Pick<Item, 'name' | 'price' | 'id' | 'gender'>;
   }
 >;
 
@@ -12,6 +12,7 @@ type ConvertedItems = {
   name: string;
   price: number;
   quantity: number;
+  gender: Gender;
 }[];
 
 export default (orderedItems: OrderedItems) =>
@@ -27,7 +28,8 @@ export default (orderedItems: OrderedItems) =>
         sizeSymbol: orderItem.sizeSymbol,
         name: orderItem.item.name,
         price: orderItem.item.price,
-        quantity: 1
+        quantity: 1,
+        gender: orderItem.item.gender
       });
     else existingItem.quantity = existingItem.quantity + 1;
 
