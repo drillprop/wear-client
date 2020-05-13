efedc604e97e7caf5db3cf90a098e87bba7af41f
drillprops <drylbartosz@gmail.com>
2020-05-13 18:47:35 +0200

    fix issues with proper alignment in tables

--- src/styles/table.styles.ts
+++ src/styles/table.styles.ts
@@ -1,9 +1,9 @@
 import styled, { css } from 'styled-components';
-import { grays } from './colors';
+import { grays, black } from './colors';
 import { montserrat, roboto } from './fonts';
 import fontSizes from './fontSizes';
 
-export const Table = styled.table<{ tableColumnNames?: string[] }>`
+export const Table = styled.table<{ tableColumnNames: string[] }>`
   width: 100%;
   margin-top: 50px;
   border-collapse: collapse;
@@ -23,26 +23,32 @@ export const Table = styled.table<{ tableColumnNames?: string[] }>`
     }
     tr {
       border: 1px solid #ccc;
+      min-height: 50px;
     }
     td {
+      min-height: 70px;
       border: none;
-      border-bottom: 1px solid #eee;
+      padding: 0;
+      border-bottom: 1px solid ${grays[5]};
       position: relative;
+      display: flex;
+      align-items: center;
+      word-break: break-all;
       padding-left: 50%;
     }
     td:before {
       position: absolute;
-      top: 0px;
-      left: 0px;
-      padding: 20px;
+      top: 50%;
+      font-weight: 700;
+      transform: translateY(-50%);
+      left: 10px;
       width: 45%;
       white-space: nowrap;
       font-size: ${fontSizes[0]};
       text-transform: uppercase;
       font-family: ${roboto};
       text-align: left;
-      background-color: ${grays[0]};
-      color: ${grays[5]};
+      color: ${black};
     }
     ${(props) =>
       props.tableColumnNames &&
