e7b00d787b2a59961a252687f2596f35b670d4c2
drillprops <drylbartosz@gmail.com>
2020-05-14 19:18:02 +0200

    add second parameter to withPrivateRoute

--- src/hoc/withPrivateRoute.tsx
+++ src/hoc/withPrivateRoute.tsx
@@ -2,7 +2,7 @@ import { NextPage } from 'next';
 import { ApolloAppContext } from 'next-with-apollo';
 import Router from 'next/router';
 import React from 'react';
-import { MeQuery } from '../generated/types';
+import { MeQuery, UserRole } from '../generated/types';
 import ME from '../graphql/queries/ME';
 
 const redirectToSign = (context: ApolloAppContext) => {
@@ -15,13 +15,24 @@ const redirectToSign = (context: ApolloAppContext) => {
   }
 };
 
-export const withPrivateRoute = (Component: NextPage<any>) => {
+export const withPrivateRoute = (
+  Component: NextPage<any>,
+  availableFor: UserRole = UserRole.CUSTOMER
+) => {
   return class PrivateRoute extends React.Component {
     static async getInitialProps(ctx: ApolloAppContext) {
       try {
         const { data } = await ctx.apolloClient.query<MeQuery>({ query: ME });
-        const user = data.me;
-        if (user) {
+
+        const requiredLevel = Object.keys(UserRole).indexOf(availableFor);
+        // -1 not signed
+        //  0 ADMIN
+        //  1 EMPLOYEE
+        //  2 CUSTOMER
+        const userLevel = Object.keys(UserRole).indexOf(data.me?.role || '');
+        const isAllowed = userLevel <= requiredLevel;
+
+        if (isAllowed) {
           return data.me;
         } else {
           redirectToSign(ctx);
