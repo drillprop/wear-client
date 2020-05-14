277bbb78c9afa1d3a29575253e436184fbd8294d
drillprops <drylbartosz@gmail.com>
2020-05-14 16:54:54 +0200

    hide certain links in MobileMenu when user is not sign in or doesn't have proper credentials, implement logout

--- src/components/Layout/layout/header/MobileMenu.tsx
+++ src/components/Layout/layout/header/MobileMenu.tsx
@@ -1,6 +1,11 @@
 import { useRouter } from 'next/router';
 import React, { useEffect, useState } from 'react';
-import { Gender } from '../../../../generated/types';
+import {
+  Gender,
+  useMeQuery,
+  UserRole,
+  useSignoutMutation,
+} from '../../../../generated/types';
 import getGenderCategories from '../../../../utils/getGenderCategories';
 import LinkAnchor from '../../../LinkAnchor/LinkAnchor';
 import ToggleableList from '../../../ToggleableList/ToggleableList';
@@ -11,12 +16,18 @@ import {
   Menu,
 } from './MobileMenu.styles';
 import { AnimatePresence } from 'framer-motion';
+import ME from '../../../../graphql/queries/ME';
 
 const MobileMenu = () => {
   const manCategories = getGenderCategories(Gender.MAN);
   const womanCategories = getGenderCategories(Gender.WOMAN);
   const [menuActive, setMenuActive] = useState(true);
   const { asPath } = useRouter();
+  const { data } = useMeQuery();
+
+  const [signOut] = useSignoutMutation({
+    refetchQueries: [{ query: ME }],
+  });
 
   useEffect(() => {
     if (menuActive) {
@@ -24,6 +35,9 @@ const MobileMenu = () => {
     }
   }, [asPath]);
 
+  const isUser = data?.me;
+  const isEmployee =
+    data?.me?.role === UserRole.EMPLOYEE || data?.me?.role === UserRole.ADMIN;
   return (
     <>
       <HamburgerButtonWrapper>
@@ -45,19 +59,25 @@ const MobileMenu = () => {
             exit={{ x: '100%', opacity: 0 }}
           >
             <ul>
-              <ToggleableList title='ACCOUNT'>
+              {isUser ? (
+                <ToggleableList title='ACCOUNT'>
+                  <li>
+                    <LinkAnchor href='/account/profile'>my profile</LinkAnchor>
+                  </li>
+                  <li>
+                    <LinkAnchor href='/account/contact'>
+                      contact details
+                    </LinkAnchor>
+                  </li>
+                  <li>
+                    <LinkAnchor href='/account/orders'>orders</LinkAnchor>
+                  </li>
+                </ToggleableList>
+              ) : (
                 <li>
-                  <LinkAnchor href='/account/profile'>my profile</LinkAnchor>
+                  <LinkAnchor href='/sign'>login</LinkAnchor>
                 </li>
-                <li>
-                  <LinkAnchor href='/account/contact'>
-                    contact details
-                  </LinkAnchor>
-                </li>
-                <li>
-                  <LinkAnchor href='/account/orders'>orders</LinkAnchor>
-                </li>
-              </ToggleableList>
+              )}
               <ToggleableList title='WOMAN'>
                 {womanCategories.map((category) => (
                   <li key={category}>
@@ -82,21 +102,23 @@ const MobileMenu = () => {
                   </li>
                 ))}
               </ToggleableList>
-              <ToggleableList title='ADMIN'>
-                <li>
-                  <LinkAnchor href='/admin/users'>users</LinkAnchor>
-                </li>
-                <li>
-                  <LinkAnchor href='/admin/items'>items</LinkAnchor>
-                </li>
-                <li>
-                  <LinkAnchor href='/admin/orders'>user's orders</LinkAnchor>
-                </li>
-              </ToggleableList>
+              {isEmployee && (
+                <ToggleableList title='ADMIN'>
+                  <li>
+                    <LinkAnchor href='/admin/users'>users</LinkAnchor>
+                  </li>
+                  <li>
+                    <LinkAnchor href='/admin/items'>items</LinkAnchor>
+                  </li>
+                  <li>
+                    <LinkAnchor href='/admin/orders'>user's orders</LinkAnchor>
+                  </li>
+                </ToggleableList>
+              )}
               <li>
                 <LinkAnchor href='/cart'>YOUR CART</LinkAnchor>
               </li>
-              <li>LOGOUT</li>
+              {isUser && <li onClick={() => signOut()}>LOGOUT</li>}
             </ul>
           </Menu>
         )}
