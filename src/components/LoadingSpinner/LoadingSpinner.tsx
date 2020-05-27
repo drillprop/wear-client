87a0a7dd5ab34589a3b83823c7c2faedff2764a6
drillprops <drylbartosz@gmail.com>
2020-05-27 23:04:22 +0200

    create LoadingSpinner component

---
+++ src/components/LoadingSpinner/LoadingSpinner.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { SpinnerWrapper } from './LoadingSpinner.styles';
+
+const Spinner = () => {
+  return (
+    <SpinnerWrapper>
+      <div />
+      <div />
+      <div />
+    </SpinnerWrapper>
+  );
+};
+
+export default Spinner;
