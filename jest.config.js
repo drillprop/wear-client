657d6a3d17f66636e0093746d0cf19d5200441bb
drillprop <drylbartosz@gmail.com>
2020-08-17 04:54:40 +0200

    setup tests for next with typescript

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.test.json',
+    },
+  },
+};
