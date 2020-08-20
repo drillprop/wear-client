0e97c90fe35af1eca2a5f58a5f91d1bbf7032dfb
drillprop <drylbartosz@gmail.com>
2020-08-21 00:08:04 +0200

    create file with constants

---
+++ src/utils/constants.ts
@@ -0,0 +1,18 @@
+import { Gender, Category, UserRole, SizeSymbol } from '../generated/types';
+
+export const GenderArr: Gender[] = ['MAN', 'WOMAN'];
+
+export const CategoryArr: Category[] = [
+  'TROUSERS',
+  'DRESS',
+  'BLOUSE',
+  'TSHIRT',
+  'SHIRT',
+  'JACKET',
+  'BLAZER',
+  'SWEATSHIRT',
+];
+
+export const SizesArr: SizeSymbol[] = ['XS', 'S', 'M', 'L', 'XL', 'XXL'];
+
+export const UserRoleArr: UserRole[] = ['ADMIN', 'EMPLOYEE', 'CUSTOMER'];
