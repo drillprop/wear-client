4886f6426dfb620069b6e740514337767592f571
drillprop <drylbartosz@gmail.com>
2020-08-21 01:33:24 +0200

    remove enum in orders page

--- src/pages/admin/orders.tsx
+++ src/pages/admin/orders.tsx
@@ -1,10 +1,9 @@
 import { NextPage } from 'next';
 import React from 'react';
-import { UserRole } from '../../generated/types';
 import { withPrivateRoute } from '../../hoc/withPrivateRoute';
 
 const OrdersPage: NextPage = () => {
   return <div>OrdersPage page</div>;
 };
 
-export default withPrivateRoute(OrdersPage, UserRole.EMPLOYEE);
+export default withPrivateRoute(OrdersPage, 'EMPLOYEE');
