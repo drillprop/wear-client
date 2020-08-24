140637a5648a506e4ab786dfe11ccceee8ba4bfe
drillprop <drylbartosz@gmail.com>
2020-08-24 18:39:09 +0200

    add test for incItem

--- src/contexts/CartContext.utils.test.ts
+++ src/contexts/CartContext.utils.test.ts
@@ -51,7 +51,7 @@ describe('addItem', () => {
       },
     ]);
   });
-  it('correctly add item to cart when the cart is not empty', () => {
+  it('correctly add item to cart when the cart is not empty and set new item at the beginning of an array', () => {
     const mockPrevCart: ICartItem[] = [
       {
         id: '123',
@@ -86,6 +86,55 @@ describe('addItem', () => {
         name: 'some nice trousers',
         price: 432,
         quantity: 1,
+        size: 'XS',
+      },
+    ]);
+  });
+});
+
+describe('incItem', () => {
+  it('increment item quantity in cart', () => {
+    const mockPrevCart: ICartItem[] = [
+      {
+        id: '12322',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'gray t-shirt',
+        price: 22,
+        quantity: 1,
+        size: 'M',
+      },
+      {
+        id: '123',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'some nice trousers',
+        price: 432,
+        quantity: 1,
+        size: 'XS',
+      },
+    ];
+    const mockItem: ICartItem = {
+      id: '123',
+      imageUrl: 'http://example.com/image.jpg',
+      name: 'some nice trousers',
+      price: 432,
+      quantity: 1,
+      size: 'XS',
+    };
+    expect(incItem(mockPrevCart, mockItem)).toStrictEqual([
+      {
+        id: '12322',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'gray t-shirt',
+        price: 22,
+        quantity: 1,
+        size: 'M',
+      },
+      {
+        id: '123',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'some nice trousers',
+        price: 432,
+        quantity: 2,
         size: 'XS',
       },
     ]);
