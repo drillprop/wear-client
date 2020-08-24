9586f3af11e485f06ccd55a5c4e7283d18062582
drillprop <drylbartosz@gmail.com>
2020-08-24 18:52:34 +0200

    add test for decrItem

--- src/contexts/CartContext.utils.test.ts
+++ src/contexts/CartContext.utils.test.ts
@@ -22,7 +22,7 @@ describe('addItem', () => {
     expect(addItem(mockPrevCart, mockNewItem)).toStrictEqual([mockNewItem]);
   });
 
-  it('correctly increase quantity of item in cart, if item in cart already exists', () => {
+  it('correctly increase quantity of an item in cart, if item in cart already exists', () => {
     const mockPrevCart: ICartItem[] = [
       {
         id: '123',
@@ -44,7 +44,7 @@ describe('addItem', () => {
       },
     ]);
   });
-  it('correctly add item to cart when the cart is not empty and set new item at the beginning of an array', () => {
+  it('correctly add an item to cart when the cart is not empty and set new item at the beginning of an array', () => {
     const mockPrevCart: ICartItem[] = [
       {
         id: '12322',
@@ -113,6 +113,79 @@ describe('incItem', () => {
         price: 432,
         quantity: 2,
         size: 'XS',
+      },
+    ]);
+  });
+});
+
+describe('decrItem', () => {
+  it('decrease item quantity in cart', () => {
+    const mockPrevCart: ICartItem[] = [
+      {
+        id: '12322',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'gray t-shirt',
+        price: 22,
+        quantity: 1,
+        size: 'M',
+      },
+      {
+        id: '123',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'some nice trousers',
+        price: 432,
+        quantity: 2,
+        size: 'XS',
+      },
+    ];
+    expect(decrItem(mockPrevCart, mockNewItem)).toStrictEqual([
+      {
+        id: '12322',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'gray t-shirt',
+        price: 22,
+        quantity: 1,
+        size: 'M',
+      },
+      {
+        id: '123',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'some nice trousers',
+        price: 432,
+        quantity: 1,
+        size: 'XS',
+      },
+    ]);
+  });
+
+  it('removes item from cart if quantity of the item is equal 1', () => {
+    const mockPrevCart: ICartItem[] = [
+      {
+        id: '12322',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'gray t-shirt',
+        price: 22,
+        quantity: 1,
+        size: 'M',
+      },
+      {
+        id: '123',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'some nice trousers',
+        price: 432,
+        quantity: 1,
+        size: 'XS',
+      },
+    ];
+    expect(decrItem(mockPrevCart, mockNewItem)).toHaveLength(1);
+    expect(decrItem(mockPrevCart, mockNewItem)).toStrictEqual([
+      {
+        id: '12322',
+        imageUrl: 'http://example.com/image.jpg',
+        name: 'gray t-shirt',
+        price: 22,
+        quantity: 1,
+        size: 'M',
       },
     ]);
   });
